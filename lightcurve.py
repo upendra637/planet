24788c07ec824e66f2e3b75c81a61600966e4043
upendra637 <upendra8063@gmail.com>
2026-05-16 06:25:01 +0530

    folding and binning

--- lightcurve.py
+++ lightcurve.py
@@ -6,18 +6,18 @@ from lightkurve import search_targetpixelfile
 # Getting the target pixel file for a star in 
 tpf = search_targetpixelfile('KIC 6922244', author = 'kepler' , quarter = 4, cadence = 'long').download()
 
-# plotting the pixel image
-tpf.plot()
-plt.show()
-
 # converting the pixel file to a lightkurve 
 lc = tpf.to_lightcurve(aperture_mask = tpf.pipeline_mask)
 
-# plotting the lightkurve 
-lc.plot()
-plt.show()
+# Flattening the lightcurve
+flattened_lc = lc.flatten(windows_length = 401)
 
 # getting the period
 p = np.linspace(1,20,1000)
-pg = lc.to_periodogram(method = 'bls' , period = p , frequency_factor = 500)
-print(pg.period_at_max_power)
+pg = flattened_lc.to_periodogram(method = 'bls' , period = p , frequency_factor = 500)
+
+# folding 
+fold_lc = flattened_lc.fold(period = pg.period_at_max_power , epoch_time = pg.transit_time_at_max_power)
+
+# binning 
+bin_lc = fold_lc(time_bin_size = 0.01)
