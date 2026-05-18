c3a6bded3cefa0b05fe55e650e14bcd4fa484ec3
upendra637 <upendra8063@gmail.com>
2026-05-18 13:51:54 +0530

    changing the  parameters

--- bman.py
+++ bman.py
@@ -4,16 +4,16 @@ import matplotlib.pyplot as plt
 
 params = batman.TransitParams()       #object to store transit parameters
 params.t0 = 0.                        #time of inferior conjunction
-params.per = 1.                      #orbital period
-params.rp = 0.05                       #planet radius (in units of stellar radii)
-params.a = 15.                        #semi-major axis (in units of stellar radii)
-params.inc = 86.8                     #orbital inclination (in degrees)
+params.per = 0.1                       #orbital period
+params.rp = 0.1                      #planet radius (in units of stellar radii)
+params.a = 16.                        #semi-major axis (in units of stellar radii)
+params.inc = 87.                    #orbital inclination (in degrees)
 params.ecc = 0.                       #eccentricity
 params.w = 90.                        #longitude of periastron (in degrees)
 params.limb_dark = "nonlinear"        #limb darkening model
 params.u = [0.5, 0.1, 0.1, -0.1]      #limb darkening coefficients [u1, u2, u3, u4]
 
-t = np.linspace(-0.025, 0.025, 1000)  #times at which to calculate light curve
+t = np.linspace(-1.5, 1.5, 1000)  #times at which to calculate light curve
 m = batman.TransitModel(params, t)    #initializes model
 
 flux = m.light_curve(params)
