c6a15c7eb2578d835e62dff8cc643009c408fb3d
upendra637 <upendra8063@gmail.com>
2026-06-29 20:45:11 +0530

    trying to fix

--- bin.py
+++ bin.py
@@ -3,25 +3,58 @@ import matplotlib.pyplot as plt
 import lightkurve as lk
 
 
-def flat_data():
-    lc = lk.search_lightcurve('KIC 6922244' , author = 'kepler' , cadence = 'long' , quarter = 4).download()
+def flat_data(
+        star,    # KIC number of the star to be analyzed or the star name
+        author,  # The author of the light curve data (e.g., 'kepler', 'tess', etc.)
+        cadence, # The cadence of the light curve data (e.g., 'long', 'short', etc.)
+        quarter, # The quarter of the light curve data to be analyzed
+        wind_len # The window length for the flattening process (default is 601)
 
-    flc = lc.flatten(window_length = 601)
+):
+    lc = lk.search_lightcurve(f'{star}' , author = author , cadence = cadence , quarter = quarter).download()
+
+    flc = lc.flatten(window_length = wind_len)
     return flc
 
-def bin_data():
+def bin_data(
+        min_period,  # Minimum period for the periodogram
+        max_period,  # Maximum period for the periodogram
+        N, # Number of period points to evaluate in the periodogram
+        bin_size # Size of the time bins for folding and binning the light curve data
+):
 
     flc = flat_data()
 
-    p = np.linspace(1,20,10000)
+    p = np.linspace(min_period, max_period, N)
     pg = flc.to_periodogram(method = 'bls', period = p, frequency_factor = 500)
 
     folc = flc.fold(period = pg.period_at_max_power, epoch_time = pg.transit_time_at_max_power)
 
-    blc = folc.bin(time_bin_size = 0.01)
+    blc = folc.bin(time_bin_size = bin_size)
 
     return blc
 
+def flat_plot():
+
+    flc = flat_data()
+    plt.figure(figsize=(10, 6))
+    flc.plot()
+    plt.title('Flattened Light Curve')
+    plt.xlabel('Time (BJD - 2454833)')
+    plt.ylabel('Normalized Flux')
+    plt.grid()
+    plt.show()
+
+def bin_plot():
+    
+    blc = bin_data()
+    plt.figure(figsize=(10, 6))
+    blc.plot()
+    plt.title('Binned Folded Light Curve')
+    plt.xlabel('Phase')
+    plt.ylabel('Normalized Flux')
+    plt.grid()
+    plt.show()
 
 
 
--- bman.py
+++ bman.py
@@ -3,51 +3,35 @@ import numpy as np
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-from lightkurve import search_targetpixelfile
 from scipy.optimize import curve_fit
 import batman
+import bin
 
 
-# Getting the target pixel file for a star in 
-tpf = search_targetpixelfile('KIC 6922244', author = 'kepler' , quarter = 4, cadence = 'long').download()
-
-# converting the pixel file to a lightkurve 
-lc = tpf.to_lightcurve(aperture_mask = tpf.pipeline_mask)
-
-# Flattening the lightcurve
-flattened_lc = lc.flatten(window_length = 401)
-
-# getting the period
-p = np.linspace(1,100,10000)
-pg = flattened_lc.to_periodogram(method = 'bls' , period = p , frequency_factor = 500)
-
-# folding 
-fold_lc = flattened_lc.fold(period = pg.period_at_max_power , epoch_time = pg.transit_time_at_max_power)
+flattened_lc = bin.flat_data('KIC 6922244', 'kepler', 'long', 4, 401)
 
 # binning 
-bin_lc = fold_lc.bin(time_bin_size = 0.01)
+bin_lc = bin.bin_data(1, 100, 10000, 0.01)
 phase = bin_lc.phase.value
 flux = bin_lc.flux.value
 
 
 # model by batman
-params = batman.TransitParams()       #object to store transit parameters
-params.t0 = 0.                        #time of inferior conjunction
-params.per = 3.522                    #orbital period
-params.rp = 0.1                       #planet radius (in units of stellar radii)
-params.a = 10.                        #semi-major axis (in units of stellar radii)
-params.inc = 87.                      #orbital inclination (in degrees)
-params.ecc = 0.                       #eccentricity
-params.w = 90.                        #longitude of periastron (in degrees)
-params.limb_dark = "quadratic"        #limb darkening model
-params.u = [0.1, 0.3]                 #limb darkening coefficients [u1, u2, u3, u4]
+
 
 def model(phase, rp, a, inc,t0):
 
-    params.a = a
-    params.rp = rp
-    params.inc = inc
-    params.t0 = t0
+    params = batman.TransitParams()       #object to store transit parameters
+
+    params.a = a                          #semi-major axis (in units of stellar radii)
+    params.rp = rp                        #planet radius (in units of stellar radii)
+    params.inc = inc                      #orbital inclination (in degrees)
+    params.t0 = t0                        #time of inferior conjunction
+    params.per = 3.522                    #orbital period
+    params.ecc = 0.                       #eccentricity
+    params.w = 90.                        #longitude of periastron (in degrees)
+    params.limb_dark = "quadratic"        #limb darkening model
+    params.u = [0.1, 0.3]                 #limb darkening coefficients [u1, u2, u3, u4]
 
     m = batman.TransitModel(params,phase)
     flux = m.light_curve(params)
